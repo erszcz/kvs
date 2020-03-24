e37e7a6410b2f88cc7069157048a8a26738de87a
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2020-03-24 10:34:11 +0100

    Implement from_proplist/1 and proplist_to_map/1

--- src/kvs.erl
+++ src/kvs.erl
@@ -1,29 +1,38 @@
 -module(kvs).
 
--export([filter/2,
-         find/2,
-         fold/3,
+-export([
          from_proplist/1,
          get/2, get/3,
-         is_key/2,
-         iterator/1,
-         keys/1,
-         map/2,
-         merge/2,
-         new/0,
-         next/1,
-         put/3,
-         remove/2,
-         size/1,
-         take/2,
-         to_list/1,
-         update/3,
-         update_with/3, update_with/4,
-         values/1,
-         with/2,
-         without/2]).
+         proplist_to_map/1
+
+         %% TODO:
+         %filter/2,
+         %find/2,
+         %fold/3,
+         %is_key/2,
+         %iterator/1,
+         %keys/1,
+         %map/2,
+         %merge/2,
+         %new/0,
+         %next/1,
+         %put/3,
+         %remove/2,
+         %size/1,
+         %take/2,
+         %to_list/1,
+         %update/3,
+         %update_with/3, update_with/4,
+         %values/1,
+         %with/2,
+         %without/2
+        ]).
 
 -type t() :: [{term(), term()}].
+
+-spec from_proplist(proplists:proplist()) -> t().
+from_proplist(Proplist) ->
+    proplists:unfold(Proplist).
 
 -spec get(Key, KVs) -> Value when
       Key :: term(),
@@ -45,3 +54,7 @@ get(Key, KVs, Default) ->
         false -> Default;
         {Key, Value} -> Value
     end.
+
+-spec proplist_to_map(proplists:proplist()) -> map().
+proplist_to_map(Proplist) ->
+    maps:from_list(from_proplist(Proplist)).
