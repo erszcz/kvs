1cf0d2aec0ca174517865b7a21441b1910de69ec
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2020-03-26 22:38:33 +0100

    Add docs

--- src/kvs.erl
+++ src/kvs.erl
@@ -1,4 +1,5 @@
 -module(kvs).
+-author("Radek Szymczyszyn <lavrin@gmail.com>").
 
 -export([
          from_proplist/1,
@@ -28,33 +29,61 @@
          %without/2
         ]).
 
--type t() :: [{term(), term()}].
+-export_type([t/0,
+              keywords/0]).
 
+-type t() :: [{term(), term()}].
+%% The default `kvs' type - a list of key-value pairs.
+
+-type keywords() :: [{atom(), term()}].
+%% An Elixir-compatible keyword list.
+%% A list of pairs in which the first item (the key) is an atom.
+
+%% @doc Unfolds a `Proplist' into a list of key-value pairs.
+%%
+%% Properties which are not pairs are substituted with pairs
+%% where the second element of each is `true'.
+%%
+%% The call fails with a `function_clause' if `Proplist' is not a list.
 -spec from_proplist(proplists:proplist()) -> t().
-from_proplist(Proplist) ->
+from_proplist(Proplist) when is_list(Proplist) ->
     proplists:unfold(Proplist).
 
+%% @doc Returns the `Value' associated with `Key' if `KVs' contains `Key'.
+%%
+%% The call fails with a `{badkey, Key}' exception if no value is associated with `Key',
+%% or with a `function_clause' if `KVs' is not a list.
 -spec get(Key, KVs) -> Value when
       Key :: term(),
       KVs :: t(),
       Value :: term().
-get(Key, KVs) ->
+get(Key, KVs) when is_list(KVs) ->
     case lists:keyfind(Key, 1, KVs) of
         false -> erlang:error({badkey, Key}, [Key, KVs]);
         {Key, Value} -> Value
     end.
 
--spec get(Key, KVs, Default) -> Value when
+%% @doc Returns `Value' associated with `Key' if `KVs' contains `Key'.
+%% If no value is associated with `Key', `Default' is returned.
+%%
+%% The call fails with a `function_clause' if `KVs' is not a list.
+-spec get(Key, KVs, Default) -> Value | Default when
       Key :: term(),
       KVs :: t(),
       Default :: term(),
       Value :: term().
-get(Key, KVs, Default) ->
+get(Key, KVs, Default) when is_list(KVs) ->
     case lists:keyfind(Key, 1, KVs) of
         false -> Default;
         {Key, Value} -> Value
     end.
 
+%% @doc Returns a map built from `Proplist'.
+%%
+%% The proplist is first unfolded, i.e. properties which are not pairs
+%% are substituted with pairs where the second element of each is `true'.
+%%
+%% The call fails with a `function_clause' if `Proplist' is not a list.
 -spec proplist_to_map(proplists:proplist()) -> map().
-proplist_to_map(Proplist) ->
+proplist_to_map(Proplist) when is_list(Proplist) ->
     maps:from_list(from_proplist(Proplist)).
